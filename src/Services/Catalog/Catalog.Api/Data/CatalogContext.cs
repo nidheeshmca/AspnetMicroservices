82f93fe022bc65ac343a4e6c740141712c5b9e11
nidheesh Kumar <nidheeshmca@gmail.com>
2021-06-16 22:34:04 +0530

    Catelog Api with

---
+++ src/Services/Catalog/Catalog.Api/Data/CatalogContext.cs
@@ -0,0 +1,23 @@
+﻿using Catalog.Api.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.Api.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+ 
+        public CatalogContext(IConfiguration config)
+        {
+            var client = new MongoClient(config.GetSection("databaseSettings")["ConnectionString"]);
+            var database = client.GetDatabase(config.GetSection("databaseSettings")["DatabaseName"]);
+            Products = database.GetCollection<Product>(config.GetSection("databaseSettings")["CollectionName"]);
+            CatelogContextSeed.Seed(Products);
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
---
+++ src/Services/Catalog/Catalog.Api/Data/ICatalogContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Catalog.Api.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.Api.Data
+{
+    public interface ICatalogContext
+    {
+        IMongoCollection<Product> Products { get; }
+    }
+}
